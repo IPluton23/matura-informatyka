cfc4cb0cc2ec9685c2bb4704ae8a0e12f733d78b
Karol <plutopradzynskikarol@gmail.com>
2024-02-18 18:15:18 +0100

    zadanie 64.3

--- zbior-zadan/zad64/zad64.py
+++ zbior-zadan/zad64/zad64.py
@@ -3,8 +3,8 @@ odp = open("wyniki_obrazki.txt", "a")
 #odp.write("Zad. 64.1:\n")
 
 
-calosc = [] # Przechowanie obrazów
-obraz = [] # Pojedynczy obraz
+calosc = []  # Przechowanie obrazów
+obraz = []  # Pojedynczy obraz
 
 # Usunięcie bitów parzystości z obrazu
 def usun_ramke(obraz):
@@ -36,8 +36,8 @@ def czyRekurencyjny(obraz):
     for i in range(20):
         # ćwiartka 1. i 2. znajdują się w tych samych wierszach, ale różnią się kolumnami. Tak samo ćwiartki 3. i 4.
         if i < 10:
-            cwiartka1[i] = obraz[i][:len(obraz)//2] # :len(obraz)//2 -> od 0 do połowy linijki obrazu
-            cwiartka2[i] = obraz[i][len(obraz)//2:] # len(obraz)//2: -> od połowy linijki obrazu do końca linijki obrazu
+            cwiartka1[i] = obraz[i][:len(obraz)//2]  # :len(obraz)//2 -> od 0 do połowy linijki obrazu
+            cwiartka2[i] = obraz[i][len(obraz)//2:]  # len(obraz)//2: -> od połowy linijki obrazu do końca linijki obrazu
         else:
             cwiartka3[i - 10] = obraz[i][:len(obraz)//2]
             cwiartka4[i - 10] = obraz[i][len(obraz)//2:]
@@ -46,6 +46,44 @@ def czyRekurencyjny(obraz):
         return True
     else:
         return False
+
+# Sprawdzenie poprawności i naprawialności obrazów (zad 64.3)
+def poprawnosc(obraz):
+    pion_obraz = [""]*20  # 20-elementowa tablica zawierająca obraz czytany od góry na dół, bez bitów parzystości wierszy
+
+    pion_bity = []  # tablica zawierająca bity parzystości z ostatniej linii obrazu
+    poziom_bity = []  # tablica zawierająca bity parzystosci z ostatniej kolumny
+
+    new_obraz = usun_ramke(obraz)
+
+    bledy_pion = 0  # ilość błędów w ostatnim wierszu obrazu
+    bledy_poziom = 0  # ilość błędów w ostatniej kolumnie obrazu
+
+    for line in obraz:
+        for i in range(20):
+            pion_obraz[i] += line[i]
+
+    for i in range(20):
+        poziom_bity.append(obraz[i][-1])
+        pion_bity.append(pion_obraz[i][-1])
+
+    for i in range(20):
+        bit1 = new_obraz[i].count("1") % 2
+        if str(bit1) != poziom_bity[i]:
+            bledy_poziom += 1
+
+        bit2 = pion_obraz[i][:20].count("1") % 2
+        if str(bit2) != pion_bity[i]:
+            bledy_pion += 1
+
+    if bledy_poziom == 0 and bledy_pion == 0:
+        return ["poprawny", bledy_pion + bledy_poziom]
+    elif bledy_poziom >1 or bledy_pion > 1:
+        return ["nienaprawialny", bledy_pion + bledy_poziom]
+    else:
+        return ["naprawialny", bledy_pion + bledy_poziom]
+
+
 
 # Przekazanie obrazów do listy calosc[]
 for line in file:
@@ -56,12 +94,18 @@ for line in file:
     obraz.append(line.strip())
 calosc.append(obraz)
 
-rewersy = [] # tablica z obrazami będącymi rewersami
-max_czarne = 0 # najwieksza liczba czarnych pikseli
+rewersy = []  # tablica z obrazami będącymi rewersami
+max_czarne = 0  # najwieksza liczba czarnych pikseli
 
-rekurencyjne = [] # tablica z obrazami rekurencyjnymi
+rekurencyjne = []  # tablica z obrazami rekurencyjnymi
 
-for obraz in calosc:
+naprawialnosc = []  # przechowuje info o poprawnosci/naprawialnosci/nienaprawialnosci i liczbie bledow kazdego z obrazka
+max_bledy = 0
+poprawne = 0
+naprawialne = 0
+nienaprawialne = 0
+
+for i, obraz in enumerate(calosc):
     # ZADANIE 1
     sprawdz = czyRewers(usun_ramke(obraz))
     if sprawdz[0]:
@@ -71,13 +115,27 @@ for obraz in calosc:
     # ZADANIE 2
     if czyRekurencyjny(usun_ramke(obraz)):
         rekurencyjne.append(obraz)
+    # ZADANIE 3
+    naprawialnosc.append(poprawnosc(obraz))
 
+
+
+for item in naprawialnosc:
+    if item[1] > max_bledy:
+        max_bledy = item[1]
+    if item[0] == "poprawny":
+        poprawne += 1
+    elif item[0] == "naprawialny":
+        naprawialne += 1
+    else:
+        nienaprawialne += 1
 
 # Zapis odpowiedzi do pliku
-odp.write("Ilość rewersów: " + str(len(rewersy)) + " najwięcej pikseli: " + str(max_czarne))
-odp.write("\n\nIlość obrazów rekurencyjnych: " + str(len(rekurencyjne)) + "\nPierwszy obrazek rekurencyjny:")
+odp.write("Ilość rewersów: {} najwięcej pikseli: {}".format(len(rewersy), max_czarne))
+odp.write("\n\nIlość obrazów rekurencyjnych: {} \nPierwszy obrazek rekurencyjny:".format(len(rekurencyjne)) )
 for line in usun_ramke(rekurencyjne[0]):
     odp.write("\n" + line)
+odp.write("\n\nIlość obrazków poprawnych: {} \nnaprawialnych: {} \nnienaprawialnych: {} \nNajwiększa ilość błędów w jednym obrazku: {}".format(poprawne, naprawialne, nienaprawialne, max_bledy))
 
 odp.close()
 file.close()
